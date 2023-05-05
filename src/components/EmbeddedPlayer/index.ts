a6f9e755de8bacfaa6de1009104ebff2baaa1802
mikhailbys <bystrovmikhailad@gmail.com>
2023-05-05 23:59:12 +0400

    EmbeddedPlayer component^ home update

---
+++ src/components/EmbeddedPlayer/index.ts
@@ -0,0 +1,1 @@
+export {EmbeddedPlayer} from './EmbeddedPlayer.view';
--- src/pages/Home/Home.view.tsx
+++ src/pages/Home/Home.view.tsx
@@ -1,9 +1,14 @@
 import {Page} from "../../components/Page";
+import {EmbeddedPlayer} from "../../components/EmbeddedPlayer";
+import './Home.styles.css';
 
 export const Home = () => {
     return (
         <Page>
-            <div>Home</div>
+            <div>
+                <h1 className='home-title'>Новое на канале</h1>
+                <EmbeddedPlayer embedId="_WlzH7Dp-vQ" />
+            </div>
         </Page>
     );
 }
